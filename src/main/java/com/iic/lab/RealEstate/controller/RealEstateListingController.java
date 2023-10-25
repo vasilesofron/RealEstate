92ca12b4e11a30a3c6eefd8b5b283dd50736aaf2
Vasile Sofron <vasilesofron01@gmail.com>
2023-10-25 15:54:19 +0300

    Added Delete listing, update and some restrictions on who can delete a listing.

--- src/main/java/com/iic/lab/RealEstate/controller/RealEstateListingController.java
+++ src/main/java/com/iic/lab/RealEstate/controller/RealEstateListingController.java
@@ -1,6 +1,8 @@
 package com.iic.lab.RealEstate.controller;
 
 import com.iic.lab.RealEstate.dto.RealEstateListingDto;
+import com.iic.lab.RealEstate.exception.ResourceNotFoundException;
+import com.iic.lab.RealEstate.exception.UnauthorizedException;
 import com.iic.lab.RealEstate.model.RealEstateListing;
 import com.iic.lab.RealEstate.model.User;
 import com.iic.lab.RealEstate.service.RealEstateListingService;
@@ -11,10 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
@@ -28,23 +27,91 @@ public class RealEstateListingController {
     private RealEstateListingService realEstateListingService;
 
 
-    // MUST WORK HERE
+    // Create Real Estate Listing
     @PostMapping("/create")
     public ResponseEntity<?> createRealEstateListing(@RequestBody @Valid RealEstateListingDto realEstateListingDto, HttpSession session){
 
+        // Get the User information based on the current session.
+
         User authenticatedUser = (User) session.getAttribute("user");
+
+        // Checking if the user is logged in.
 
         if(authenticatedUser == null){
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in.");
         }
 
+        // Create the Real Estate Listing with the DTO and the AuthenticatedUser.
 
         RealEstateListing createdListing = realEstateListingService.createRealEstateListing(realEstateListingDto, authenticatedUser);
+
+        // Returning a message based on the value of createdListing.
 
         if(createdListing != null){
             return ResponseEntity.status(HttpStatus.CREATED).body("Real estate listing created.");
         } else{
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to create real estate listing.");
+        }
+    }
+
+    // Edit Real Estate Listing
+    @PutMapping("/update/{realEstateListingId}")
+    public ResponseEntity<?> updateRealEstateListing(@PathVariable Long realEstateListingId, @RequestBody @Valid RealEstateListingDto updatedRealEstateListingDto, HttpSession session){
+
+        // Get the User details based on the session.
+
+        User authenticatedUser = (User) session.getAttribute("user");
+
+        // Checking if the user is logged in.
+
+        if(authenticatedUser == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in.");
+        }
+
+        // Updating the Real Estate Listing
+
+        try {
+            // Checking if the user who tries to modify the Real Estate Listing is the one who created it.
+            if(!realEstateListingService.isUserOwnerOfRealEstateListinig(realEstateListingId, authenticatedUser.getId())){
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to modify this listing.");
+            }
+            RealEstateListing updatedRealEstateListing = realEstateListingService.updateRealEstateListing(realEstateListingId, updatedRealEstateListingDto, authenticatedUser);
+            return ResponseEntity.ok("Real estate listing updated successfully.");
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Real estate listing not found.");
+        } catch (UnauthorizedException e){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have permission to edit this listing.");
+        }
+    }
+
+    // Deleting a Real Estate Listing
+    @DeleteMapping("/delete/{realEstateListingId}")
+    public ResponseEntity<?> deleteRealEstateListing(@PathVariable Long realEstateListingId, HttpSession session){
+
+        // Get the user details based on the session.
+
+        User authenticatedUser = (User) session.getAttribute("user");
+
+        // Check if the user is logged in.
+
+        if(authenticatedUser == null){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not logged in.");
+        }
+
+        // Delete the Real Estate Listing.
+
+        try {
+            // Checking if the user who tries to delete the Real Estate Listing is the one who created it.
+            if(!realEstateListingService.isUserOwnerOfRealEstateListinig(realEstateListingId, authenticatedUser.getId())){
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have access to delete this listing.");
+            }
+            realEstateListingService.deleteRealEstateListing(realEstateListingId, authenticatedUser);
+            return ResponseEntity.ok("Real Estate Listing delete successfully.");
+
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Real Estate listing does not exist.");
+        } catch (UnauthorizedException e){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not allowed to delete this listing.");
         }
     }
 }
--- src/main/java/com/iic/lab/RealEstate/service/RealEstateListingService.java
+++ src/main/java/com/iic/lab/RealEstate/service/RealEstateListingService.java
@@ -1,6 +1,7 @@
 package com.iic.lab.RealEstate.service;
 
 import com.iic.lab.RealEstate.dto.RealEstateListingDto;
+import com.iic.lab.RealEstate.exception.ResourceNotFoundException;
 import com.iic.lab.RealEstate.exception.UnauthorizedException;
 import com.iic.lab.RealEstate.model.RealEstateListing;
 import com.iic.lab.RealEstate.model.User;
@@ -23,12 +24,6 @@ public class RealEstateListingService {
 
     public RealEstateListing createRealEstateListing(RealEstateListingDto realEstateListingDto, User user){
 
-        /*User authenticatedUser = (User) session.getAttribute("user");
-
-        if(authenticatedUser == null){
-            throw new UnauthorizedException("User not logged in.");
-        }
-        */
         // We create the Real Estate Listing, without the creator.
         RealEstateListing realEstateListing = new RealEstateListing(
                 realEstateListingDto.getName(),
@@ -49,5 +44,56 @@ public class RealEstateListingService {
         // We save the Real Estate Listing to the repository.
         return realEstateListingRepository.save(realEstateListing);
     }
+
+    public RealEstateListing updateRealEstateListing(Long realEstateListingId, RealEstateListingDto updatedRealEstateListingDto, User authenticatedUser){
+
+        // Check if the Real Estate Listing exists in the database.
+
+        RealEstateListing existingRealEstateListing = realEstateListingRepository.findById(realEstateListingId)
+                .orElseThrow(() -> new ResourceNotFoundException("Real estate listing not found."));
+
+        // Update the Real Estate Listing
+
+        existingRealEstateListing.setName(updatedRealEstateListingDto.getName());
+        existingRealEstateListing.setDescription(updatedRealEstateListingDto.getDescription());
+        existingRealEstateListing.setAddress(updatedRealEstateListingDto.getAddress());
+        existingRealEstateListing.setPrice(updatedRealEstateListingDto.getPrice());
+        existingRealEstateListing.setUtilitySize(updatedRealEstateListingDto.getUtilitySize());
+        existingRealEstateListing.setTotalSize(updatedRealEstateListingDto.getTotalSize());
+        existingRealEstateListing.setBedroomNumber(updatedRealEstateListingDto.getBedroomNumber());
+        existingRealEstateListing.setBathroomNumber(updatedRealEstateListingDto.getBathroomNumber());
+        existingRealEstateListing.setListingType(updatedRealEstateListingDto.getListingType());
+        existingRealEstateListing.setConstructionYear(updatedRealEstateListingDto.getConstructionYear());
+
+        // Save the update in the RealEStateRepository
+
+        return realEstateListingRepository.save(existingRealEstateListing);
+
+    }
+
+    public boolean isUserOwnerOfRealEstateListinig(Long realEstateListingId, Long userId){
+        RealEstateListing existingRealEstateListing = realEstateListingRepository.findById(realEstateListingId)
+                .orElseThrow(() -> new ResourceNotFoundException("Real estate listing not found."));
+
+        return existingRealEstateListing.getCreator().getId().equals(userId);
+
+    }
+
+
+
+    // Delete a Real Estate Listing
+    public void deleteRealEstateListing(Long realEstateListingId, User authenticatedUser){
+
+        // Checking if the Real Estate Listing exists.
+
+        RealEstateListing existingRealEstateListing = realEstateListingRepository.findById(realEstateListingId)
+                .orElseThrow(() -> new ResourceNotFoundException("The real estate listing not found."));
+
+        // Deleting the Real Estate Listing
+
+        realEstateListingRepository.delete(existingRealEstateListing);
+
+    }
+
 
 }
